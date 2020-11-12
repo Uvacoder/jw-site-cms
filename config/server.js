62e6b26ecd65231a50f252d0c664cd800161d26b
digthedill <popssly@gmail.com>
2020-11-12 12:56:13 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31799dea1f37fa96cfd3c4ee25268d29'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10e562e3-4602-40b8-a1be-166fd80dd86d'
+};
